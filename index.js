ea6717bba2ed5c8a856d9514a5b7e33f4810266c
Filip Dvorak <dvof01@vse.cz>
2023-12-15 15:05:53 +0100

    added card template and styles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function generate() {
     const photoApiKey = 'QuD0hiQZNs0X9udPoRDInjSeoUehIBbmSpo0DQ2FxyYh0yC3XJ2CkAHx';
-    const prompt = "happiness";
+    var dropdown = document.getElementById("promptDropdown");
+    var prompt = dropdown.options[dropdown.selectedIndex].value;
     const imagePool = 20;
     const photoApiUrl = 'https://api.pexels.com/v1/search?query=' + prompt + '&per_page=' + imagePool;
 
@@ -15,13 +16,14 @@ function generate() {
             console.log(data);
             let imageSource = data.photos[Math.floor(Math.random() * imagePool)].src.landscape;
             console.log(imageSource)
-            document.getElementById("backgroundImage").src = imageSource;
+            document.getElementById("card").style.backgroundImage= "url(" + imageSource + ")";
         })
         .catch(error => {
             console.error('Error:', error);
         });
 
-    var element = document.getElementById("citat");
+    var citatElement = document.getElementById("citat");
+    var authorElement = document.getElementById("author");
     $.ajax({
         method: 'GET',
         url: 'https://api.api-ninjas.com/v1/quotes?category=' + prompt,
@@ -29,7 +31,9 @@ function generate() {
         contentType: 'application/json',
         success: function (result) {
             console.log(result[0].quote);
-            element.innerHTML = result[0].quote + "<br> - " + result[0].author;
+            citatElement.innerHTML = result[0].quote;
+            authorElement.innerHTML = "- " +result[0].author;
+
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
